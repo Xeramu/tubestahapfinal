41a30208ee4b49522cd3cb1a36ea85825bf7942a
Xeramu <umarexandromeda@upi.edu>
2026-06-18 20:43:08 +0700

    Delete gudang-service directory

--- gudang-service/mq/event.go
+++
@@ -1,8 +0,0 @@
-package mq
-
-type DeliveryEvent struct {
-	Resi          string `json:"resi"`
-	WarehouseZone string `json:"warehouse_zone"`
-	CourierID    int    `json:"courier_id"`
-	AssignedZone string `json:"assigned_zone"`
-}
