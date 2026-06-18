6d6f912d56c10db4a8863e5b5bac00eed3db02d0
Xeramu <umarexandromeda@upi.edu>
2026-06-18 20:44:11 +0700

    Delete payment-service directory

--- payment-service/repository.go
+++
@@ -1,6 +0,0 @@
-package main
-
-type PaymentRepository interface {
-	// Pastikan ada cetak biru method ini (sesuaikan dengan kode yang sudah kamu punya)
-	SaveTransaction(txID string, orderID int, amount int) error
-}
